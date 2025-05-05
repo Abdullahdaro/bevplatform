b046195171798b92d0da259caf609dae1ae705ef
farouk46015 <farouk46015@gmail.com>
2025-05-05 20:00:57 +0000

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import Hero from "@sections/Hero";
+import HowItWorks from "@sections/HowItWorks";
+import StepsSection from "@sections/StepsSection";
+import FAQ from "@sections/FAQ";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      home page
+    <div className="">
+       <Hero />
+        <HowItWorks />
+        <StepsSection />
+        <FAQ />
     </div>
   );
 }
